5364df7c2ae785871551b1f703702f6f77cbcf65
Yoan Hernalesteen <yoan.hernalesteen@gmail.com>
2025-12-02 15:50:46 +0100

    added routes

--- app/controllers/seen_movies_controller.rb
+++ app/controllers/seen_movies_controller.rb
@@ -5,7 +5,14 @@ class SeenMoviesController < ApplicationController
     @seenMovie.save
   end
 
+  def destroy
+    @seenMovie = SeenMovie.new(seenMovie_params)
+    @seenMovie.save
+  end
+
   private
+
+  def set_
 
   def seenMovie_params
     params.require(:seenMovie).permit(:movie_id, :user_id)
