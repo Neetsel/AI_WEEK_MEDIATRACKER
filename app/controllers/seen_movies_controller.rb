113b3bae7a2413917f410766e17288ae4efdde1f
Yoan Hernalesteen <yoan.hernalesteen@gmail.com>
2025-12-02 15:56:51 +0100

    cleaned destroy

--- app/controllers/seen_movies_controller.rb
+++ app/controllers/seen_movies_controller.rb
@@ -5,14 +5,7 @@ class SeenMoviesController < ApplicationController
     @seenMovie.save
   end
 
-  def destroy
-    @seenMovie = SeenMovie.new(seenMovie_params)
-    @seenMovie.save
-  end
 
-  private
-
-  def set_
 
   def seenMovie_params
     params.require(:seenMovie).permit(:movie_id, :user_id)
