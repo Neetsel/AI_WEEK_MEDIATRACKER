a3431ae6790e8c2011b9a17e631d8acde24e88b9
Yoan Hernalesteen <yoan.hernalesteen@gmail.com>
2025-12-02 14:23:11 +0100

    dropped and renamed database

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "ai_we_ek_mediatracker_production"
+  # config.active_job.queue_name_prefix = "ai_week_mediatracker_production"
 
   config.action_mailer.perform_caching = false
 
