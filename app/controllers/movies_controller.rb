7f6845c1456fd756a7cd00993aa11c8451f65e0a
Julien Balsiere <julien.balsiere@gmail.com>
2025-12-02 15:13:04 +0100

    method show to movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,12 @@
 class MoviesController < ApplicationController
+  befor_action :set_movie, only: [:show]
 
+  def show
+  end
+
+  private
+
+  def set_movies
+    @movie = Movie.find(params[:id])
+  end
 end
