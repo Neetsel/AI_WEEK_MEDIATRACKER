8de2e431d41d030f915bf06d983484234606148e
Yoan Hernalesteen <yoan.hernalesteen@gmail.com>
2025-12-03 16:45:22 +0100

    message controller create method done?

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,50 @@
 class MessagesController < ApplicationController
-  SYSTEM_PROMPT = "You are an expert cinephiles.\n\nI am a movie fan, looking for new movies to watch.\n\nHelp me find new movies with similar genre, themes or people involved in the making of those movies.\n\nAnswer concisely in Markdown."
+  SYSTEM_PROMPT = "You are an expert cinephile.\n\nI am a movie fan, looking for new movies to watch.\n\nHelp me find new movies with similar genres, themes or people involved in the making of those movies.\n\nAnswer concisely in Markdown."
+
+  def create
+    @chat = Chat.find(params[:id])
+    @movie = @chat.movie
+    @message = Message.new(message_params)
+    @message.chat = @chat
+    @message.role = "user"
+
+    if @message.save
+      response = @ruby_llm_chat.with_instructions(instructions).ask(@message.content)
+      @chat.messages.create(role: "assistant", content: response.content)
+
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to chat_path(@chat) }
+      end
+    else
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("new_message_container", partial: "messages/form", locals: { chat: @chat, message: @message }) }
+        format.html { render "chats/show", status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # def build_conversation_history
+  #   @chat.messages.each do |message|
+  #     @ruby_llm_chat.add_message(message)
+  #   end
+  # end
 
   private
 
- 
+  def movie_title
+    "Here is the title of the movie: #{@movie.title}."
+  end
+
+  def movie_description
+    "Here is the description of the movie: #{@movie.description}."
+  end
+
+  def instructions
+    [SYSTEM_PROMPT, movie_title, movie_description].compact.join("\n\n")
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
