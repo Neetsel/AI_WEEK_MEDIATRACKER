eb3a438bb0624ace0b0d37aa3e5732aec7de0ce2
Julien Balsiere <julien.balsiere@gmail.com>
2025-12-04 13:00:43 +0100

    connect omdbapi to movie controller with form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,6 +9,24 @@ class MoviesController < ApplicationController
   def show
   end
 
+  def create_from_omdb
+    omdb = OmdbService.new
+    response = omdb.search_by_title(params[:title])
+    if response["Response"] == "True"
+      movie = Movie.create(
+        title: response["Title"],
+        release_date: response["Year"],
+        genres: response["Genre"],
+        directors: response["Director"],
+        description: response["Plot"],
+        # poster_url: response["Poster"]
+      )
+      redirect_to movie, notice: "Movie added"
+    else
+      redirect_to movies_path, alert: "Film not available."
+    end
+  end
+
   private
 
   def set_movie
--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,10 @@ Rails.application.routes.draw do
     resources :planned_movies, only: [:create]
     resources :likes, only: [:create]
 
+    collection do
+      post :create_from_omdb
+    end
+
   end
   # Defines the root path route ("/")
   # root "posts#index"
