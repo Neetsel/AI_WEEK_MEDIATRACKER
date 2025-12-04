688c64f30ac7ce406205c461cc5482832e669956
Yoan Hernalesteen <55947537+Neetsel@users.noreply.github.com>
2025-12-04 16:08:34 +0100

    Update movie.rb
    added title to uniqueness

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,5 +3,5 @@ class Movie < ApplicationRecord
   has_many :planned_movies, dependent: :destroy
   has_many :seen_movies, dependent: :destroy
   has_many :chats, dependent: :destroy
-  validates :movie_id, uniqueness: { scope: [:release_date, :description], message: "You can't add the same movie twice." }
+  validates :title, uniqueness: { scope: [:release_date, :description], message: "You can't add the same movie twice." }
 end
