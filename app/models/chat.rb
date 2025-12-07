27a2ec70187b23589d9c3b23f9330cdacd231371
Yoan Hernalesteen <yoan.hernalesteen@gmail.com>
2025-12-07 13:31:31 +0100

    sending list of seen, liked and planned movies

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -3,4 +3,7 @@ class Chat < ApplicationRecord
   belongs_to :movie
 
   has_many :messages, dependent: :destroy
+  has_many :likes, through: :users
+  has_many :planned_movies, through: :users
+  has_many :seen_movies, through: :users
 end
